4e2467a38ff28ee094be0ad485d2dea9f2282110
jaimicore <j.a.castro.mondragon@gmail.com>
2023-05-15 10:43:16 +0200

    Added function to map clusters in JSON file

--- R/General_utils.R
+++ R/General_utils.R
@@ -426,3 +426,159 @@ draw.heatmap.motifs <- function(dist.table    = NULL,
   
   return(ht1)
 }
+
+
+########################################################
+## As the JSON tree order is not the same as the hclust tree
+## the JSON tree must be explored in order to rename the branches
+## and thus can assign a name which will be used to add the
+## branch consensuses (in the perl code)
+identify.JSON.tree.branches <- function(htree,
+                                        description.table) {
+  
+  # htree             <- results.list$All_motifs_tree
+  # description.table <- results.list$Motif_info_tab
+  # description.table$n <- 1:nrow(description.table)
+  
+  ## Change the labels for the numbers in the description table
+  htree$labels <- NULL
+  htree$labels <- as.vector(description.table$n)
+  
+  ## Erase the (',')
+  jsonTree <- convert.hclust.to.JSON(htree)
+  jsonTree <- gsub("\\],", "\\]", jsonTree, perl = TRUE)
+  jsonTree <- gsub("\n\"order\":\\s+\\d+", "", jsonTree, perl = TRUE)
+  
+  ## Save just the numbers on each branch of the tree
+  copy.Json <- jsonTree
+  copy.Json <- gsub("[^ \\d+ \\[ \\]  \\.]", "", copy.Json, perl = TRUE)
+  copy.Json <- gsub("(\\d+)", "|\\1", copy.Json, perl = TRUE)
+  
+  ## Convert the string in a vector, save those positions with
+  ## either a number, "[" or "]"
+  copy.Json.vector <- unlist(strsplit(copy.Json, " "))
+  copy.Json.vector <- copy.Json.vector[2:length(copy.Json.vector)]
+  copy.Json.vector <- copy.Json.vector[which(copy.Json.vector != "")]
+  
+  ## Saves the positions with a "[", except the first one,
+  ## they will be used to find the subgroups within the tree
+  symbol.counter <- length(which(copy.Json.vector == "["))
+  symbol <- which(copy.Json.vector == "[")[2:symbol.counter]
+  
+  
+  ## A dataframe will be created using this variables (col1 and col2)
+  ## They are created outside the loops and are treated as global variables
+  col1 <- NULL
+  col2 <- NULL
+  up.pos <- 1
+  sapply(1:(symbol.counter - 1), function(j) {
+    
+    ## Counter of condition (i == j)
+    cond.counter <<- 0
+    col1 <<- append(col1, j)
+    up.count <- 0
+    down.count <- 0
+    sapply(symbol[j]:length(copy.Json.vector) ,function(i) {
+      
+      ## Here the code search for the [] in order to identify the
+      ## subgroups and then can re-name them similarly to the hclust object
+      if (copy.Json.vector[i] == "[") {
+        up.count <<- up.count + 1
+        
+        ## Positions up
+        if (up.count == 1) {
+          up.pos <<- i
+        }
+      } else if (copy.Json.vector[i] == "]") {
+        down.count <<- down.count + 1
+      }
+      
+      ## Condition
+      if (up.count == down.count) {
+        
+        cond.counter <<- cond.counter + 1
+        
+        if (cond.counter == 1) {
+          ## Position down
+          down.pos <- i
+          temp      <- NULL
+          temp      <- gsub("[^\\d+ \\|]", "" ,copy.Json.vector[up.pos:down.pos], perl = TRUE)
+          numb      <- NULL
+          numb      <- as.integer(unlist((strsplit(paste(temp, collapse = ""), "\\|"))))
+          numb      <- numb[which(numb != "NA")]
+          id.from.n <- paste(sort(subset(description.table, n %in% numb)$id), collapse = ",")
+          # col2 <<- append(col2, paste(get.id(numb), collapse = ","))
+          col2 <<- append(col2, id.from.n)
+        }
+      }
+    })
+  })
+  
+  ## Create the dataframe and rename the columns
+  JSON.clusters.table <- data.frame(col1)
+  JSON.clusters.table$cluster <- col2
+  colnames(JSON.clusters.table) <- c("level", "cluster")
+  
+  # Until this part works well
+  leaves.in.tree.ids <- leaves.per.node(results.list$All_motifs_tree)
+  leaves.in.tree.ids.string <- sapply(leaves.in.tree.ids, function(x){
+    paste(sort(subset(description.table, n %in% x)$id), collapse = ",")
+  })
+  df <- data.table(cluster = leaves.in.tree.ids.string) |> mutate(node = 1:length(leaves.in.tree.ids.string))
+
+  
+  JSON.clusters.table <- merge(x = JSON.clusters.table, y = df, by = "cluster") |> 
+          select("level", "cluster", "node")
+
+  # cluster <- NULL
+  # # x <- 3
+  # # y <- 11
+  # sapply(1:nrow(JSON.clusters.table), function(x){
+  #   
+  #   ## Save the Ids corresponding to each branch of the JSON tree
+  #   leaves.JSON <- unlist(strsplit(JSON.clusters.table[x,2], ","))
+  #   
+  #   sapply(1:nrow(htree$merge), function(y) {
+  #     
+  #     cond.counter <<- 0
+  #     ## Save the Ids corresponding to each branch of the hclust tree
+  #     leaves.merge <- NULL
+  #     leaves.merge <- sort(leaves.per.node(htree)[[y]])
+  #     
+  #     print(x)
+  #     print(y)
+  #     if (length(leaves.JSON) == length(leaves.merge)) {
+  #       
+  #       rr <- sort(subset(description.table, n %in% leaves.merge)$id)[1]
+  #       print(rr)
+  #       print("_________________")
+  #       
+  #       
+  #       
+  #       if (rr == sort(leaves.JSON)[1]) {
+  #       # if(sort(get.id(leaves.merge))[1] == sort(leaves.JSON)[1]){
+  #     
+  #         cond.counter <<- cond.counter + 1
+  #         
+  #         if (cond.counter == 1) {
+  #           cluster <<- append(cluster, paste("node_", y, sep = ""))
+  #         }
+  #       }
+  #     }
+  #   })
+  # })
+  # 
+  # JSON.clusters.table$node <- cluster
+  return(JSON.clusters.table)
+}
+
+
+# identify.JSON.tree.branches(htree             = results.list$All_motifs_tree,
+#                             description.table = results.list$Motif_info_tab)
+
+
+
+
+  # htree             <- results.list$All_motifs_tree
+  # description.table <- results.list$Motif_info_tab
+  # description.table$n <- 1:nrow(description.table)
