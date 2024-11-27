33de5b40b41b25cb401a4e33181ced18ce567d4f
ievarau <ievarau@uio.no>
2024-11-27 11:43:46 +0100

    Updated container to be built from r-base

--- docker/requirements.R
+++ docker/requirements.R
@@ -1,20 +1,23 @@
 ##############################################################
 ## List of R packages requires in the 'minimal_output' mode ##
 ##############################################################
-.libPaths( c( "/opt/software" , .libPaths() ) )
+# .libPaths( c( "/opt/software" , .libPaths() ) )
 
 ## data.table is installed from source using a patched file
 
-install.packages(
-  "/opt/data.table_1.14.8.tar.gz", 
-  repos = NULL, 
-  type = "source",
-  lib = "/opt/software")
+# install.packages(
+#   "data.table_1.14.8.tar.gz", 
+#   repos = NULL, 
+#   type = "source")
+  # type = "source",
+  # lib = "/opt/software")
 
 ## CRAN packages:
 required_packages_cran = c(
   "BiocManager",   # To install bioconductor packages
+  "curl",
   "dplyr",          # Data manipulation
+  "data.table",
   "furrr",          # Run functions in parallel
   "optparse",       # Read command-line arguments
   "purrr",          # Iterations
@@ -32,11 +35,16 @@ required_packages_cran = c(
   "flexclust",      # Calculate adjusted rand index
   "htmlwidgets",    # Save plotly output as html
   "plotly",         # Interactive plots
-  "svglite",        # Easy export of ggplot content as svg
+  # "svglite",        # Easy export of ggplot content as svg
+  # "systemfonts",
   "jsonlite")       # To create the JSON file from the hclust outputs
 
 message("; Installing these R packages from CRAN repository: ", required_packages_cran)
-install.packages(required_packages_cran, repos="https://cran.uib.no/", lib="/opt/software")
+install.packages(
+  required_packages_cran,
+  dependencies = TRUE)
+  # repos = "https://cran.uib.no/", 
+  # lib = "/opt/software")
 
 ## Bioconductor packages:
 required_packages_bioconductor <- c(
@@ -44,4 +52,7 @@ required_packages_bioconductor <- c(
   "ComplexHeatmap")
 
 message("; Installing these R Bioconductor packages: ", required_packages_bioconductor)
-BiocManager::install(required_packages_bioconductor, lib="/opt/software")
+BiocManager::install(
+  required_packages_bioconductor)
+  # required_packages_bioconductor,
+  # lib = "/opt/software")
