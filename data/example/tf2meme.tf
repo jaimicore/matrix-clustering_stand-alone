a0a5f523962c8347dbb46b05cdd73c3d37b977f4
jaimicore <j.a.castro.mondragon@gmail.com>
2025-01-10 20:54:00 +0100

    One site motif

---
+++ data/example/tf2meme.tf
@@ -0,0 +1,34 @@
+AC  oligos_7nt_mkv1_m2
+XX
+ID  oligos_7nt_mkv1_m2oligos_7nt_mkv1_m2
+XX
+DE  aggaacataaat
+P0           a         c         g         t
+1            1         0         0         0
+2            0         0         1         0
+3            0         0         1         0
+4            1         0         0         0
+5            1         0         0         0
+6            0         1         0         0
+7            1         0         0         0
+8            0         0         0         1
+9            1         0         0         0
+10           1         0         0         0
+11           1         0         0         0
+12           0         0         0         1
+XX
+BA  1 sequences
+XX
+BS  AGGAACATAAAT; site_0; 1; 12; 0; p
+CC  program: feature
+CC  matrix.nb: 2
+CC  matrix.nb: 2
+CC  sites: 1
+CC  consensus.strict: aggaacataaat
+CC  consensus.strict.rc: ATTTATGTTCCT
+CC  consensus.IUPAC: aggaacataaat
+CC  consensus.IUPAC.rc: ATTTATGTTCCT
+CC  consensus.regexp: aggaacataaat
+CC  consensus.regexp.rc: ATTTATGTTCCT
+XX
+//
